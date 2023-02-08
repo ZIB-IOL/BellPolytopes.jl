88821a08b3ac9ed91453dd2da25f0383c048de9b
Sébastien Designolle <55443987+sebastiendesignolle@users.noreply.github.com>
2023-02-08 15:12:25 +0100

    Formatter (#2)
    Co-authored-by: Designolle <sdesigno@bollobas.zib.de>

--- examples/CHSH.jl
+++ examples/CHSH.jl
@@ -4,9 +4,10 @@ using LinearAlgebra
 
 N = 2 # bipartite scenario
 verticesA = [1 0 0; 0 0 1] # Bloch vectors of the measurements to be performed on Alice's side
-verticesB = [1 0 1; 1 0 -1]/sqrt(2) # Bloch vectors of the measurements to be performed on Bob's side
+verticesB = [1 0 1; 1 0 -1] / sqrt(2) # Bloch vectors of the measurements to be performed on Bob's side
 rho = rho_singlet() # shared state
-_, lower_bound, upper_bound, local_model, bell_inequality, _ = nonlocality_threshold([verticesA, verticesB], N; rho=rho)
+_, lower_bound, upper_bound, local_model, bell_inequality, _ =
+    nonlocality_threshold([verticesA, verticesB], N; rho=rho)
 
 println("Correlation matrix")
 p = correlation_tensor([verticesA, verticesB], N; rho=rho, marg=false)
@@ -18,7 +19,10 @@ println("Lower bound")
 println(lower_bound) # 0.7071
 println("Local model")
 display(local_model.x)
-println(local_model.x == sum(local_model.weights[i]*local_model.atoms[i] for i = 1:length(local_model))) # true
+println(
+    local_model.x ==
+    sum(local_model.weights[i] * local_model.atoms[i] for i in 1:length(local_model)),
+) # true
 
 println()
 
--- examples/Werner.jl
+++ examples/Werner.jl
@@ -7,6 +7,14 @@ N = 2 # bipartite scenario
 vertices = polyhedronisme("polyhedronisme-SASuSAuO.obj", 33) # Bloch vectors of the measurements to be performed by all parties
 rho = rho_singlet() # shared state
 p = correlation_tensor(vertices, N; rho=rho, marg=false)
-x, ds, primal, dual_gap, traj_data, as, M, β = bell_frank_wolfe(p; v0=1/sqrt(2), verbose=3, epsilon=1e-4, lazy_tolerance=0.5, mode_last=0, nb_last=10^6)
+x, ds, primal, dual_gap, traj_data, as, M, β = bell_frank_wolfe(
+    p;
+    v0=1 / sqrt(2),
+    verbose=3,
+    epsilon=1e-4,
+    lazy_tolerance=0.5,
+    mode_last=0,
+    nb_last=10^6,
+)
 
 println()
