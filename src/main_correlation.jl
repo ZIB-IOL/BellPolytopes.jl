7c97298a62f69a1d9b6910e187847c9072cf38a7
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-06-28 11:53:08 +0200

    Separate correlation and probability

---
+++ src/main_correlation.jl
@@ -0,0 +1,325 @@
+"""
+Calls the lazy pairwise blended conditional gradient algorithm from Frank-Wolfe package.
+
+Arguments:
+ - `p`: a correlation tensor of order `N`.
+
+Returns:
+ - `x`: a correlation tensor of order `N`, the output of the Frank-Wolfe algorithm,
+ - `ds`: a deterministic strategy, the atom returned by the last LMO,
+ - `primal`: `½|x-v₀*p|²`
+ - `dual_gap`: `⟨x-v₀*p, x-ds⟩`
+ - `active_set`: all deterministic strategies used for the decomposition of the last iterate `x`, contains fields `weights`, `atoms`, and `x`,
+ - `M`: a Bell inequality, meaningful only if the dual gap is small enough
+ - `β`: the local bound of the inequality parametrised by `M`, reliable only if the last LMO is exact.
+
+Optional arguments:
+ - `marg`: a boolean, indicates if `p` contains marginals
+ - `v0`: the visibility used to make a nonlocal `p` closer to the local polytope,
+ - `epsilon`: the tolerance, used as a stopping criterion (when the primal value or the dual gap go below its value), by default 1e-7,
+ - `verbose`: an integer, indicates the level of verbosity from 0 to 3,
+ - `shr2`: the potential underlying shrinking factor, used to display the lower bound in the callback,
+ - `mode`: an integer, 0 is for the heuristic LMO, 1 for the enumeration LMO,
+ - `nb`: an integer, number of random tries in the LMO, if heuristic, by default 10^2,
+ - `TL`: type of the last call of the LMO,
+ - `mode_last`: an integer, mode of the last call of the LMO, -1 for no last call
+ - `nb_last`: an integer, number of random tries in the last LMO, if heuristic, by default 10^5,
+ - `sym`: a boolean, indicates if the symmetry of the input should be used, by default automatic choice,
+ - `use_array`: a boolean, indicates to store the full deterministic strategies to trade memory for speed in multipartite scenarios,
+ - `callback_interval`: an integer, print interval if `verbose` = 3,
+ - `seed`: an integer, the initial random seed.
+"""
+function bell_frank_wolfe_correlation(
+    p::Array{T, N};
+    marg::Bool=N != 2,
+    v0=one(T),
+    epsilon=1e-7,
+    verbose=0,
+    shr2=NaN,
+    mode::Int=0,
+    nb::Int=10^2,
+    TL::DataType=T,
+    mode_last::Int=mode,
+    nb_last::Int=10^5,
+    epsilon_last=0,
+    sym::Union{Nothing, Bool}=nothing,
+    reynolds::Function=identity,
+    reduce::Function=(x, lmo) -> FrankWolfe.SymmetricArray(x, x[:]),
+    inflate::Function=(x, lmo) -> copyto!(x.data, x.vec),
+    use_array::Union{Nothing, Bool}=nothing,
+    active_set=nothing, # warm start
+    lazy::Bool=true, # default in FW package is false
+    max_iteration::Int=10^7, # default in FW package is 10^4
+    recompute_last_vertex=false, # default in FW package is true
+    callback_interval::Int=verbose > 0 ? 10^4 : typemax(Int),
+    renorm_interval::Int=10^3,
+    hyperplane_interval::Int=verbose > 0 ? 10callback_interval : typemax(Int),
+    bound_interval::Int=verbose > 0 ? 10callback_interval : typemax(Int),
+    nb_increment_interval::Int=verbose > 0 ? 10callback_interval : typemax(Int),
+    save_interval::Int=verbose > 0 ? 100callback_interval : typemax(Int),
+    save::Bool=false,
+    file=nothing,
+    seed::Int=0,
+    kwargs...,
+) where {T <: Number} where {N}
+    Random.seed!(seed)
+    if verbose > 0
+        println("Visibility: ", v0)
+    end
+    if use_array === nothing
+        use_array = N > 2 || reynolds !== identity
+    end
+    # symmetry detection
+    if reynolds === identity
+        if all(diff(collect(size(p))) .== 0) && p ≈ reynolds_permutedims(p, BellCorrelationsLMO(p))
+            reynolds = reynolds_permutedims
+            if sym === nothing # respect the user choice if sym is false
+                sym = true
+            end
+        else
+            if sym == true
+                @warn "Input array seemingly inconsistant with sym being true"
+            else
+                sym = false
+            end
+        end
+    else
+        if p ≈ reynolds(p, BellCorrelationsLMO(p))
+            sym = true
+        else
+            @warn "Input array seemingly inconsistant with the reynolds operator provided"
+        end
+        if use_array != true
+            @warn "For custom reynolds operators, use_array should be set to true"
+        end
+    end
+    if verbose > 1
+        println(" Symmetric: ", sym)
+    end
+    # nb of inputs
+    if verbose > 1
+        m = all(diff(collect(size(p))) .== 0) ? size(p)[end] : size(p)
+        println("   #Inputs: ", marg ? m .- 1 : m)
+        if all(diff(collect(size(p))) .== 0) && (reynolds === identity || reynolds === permutedims)
+            println(" Dimension: ", sym ? marg ? sum(binomial(m+n-2, n) for n in 1:N) : binomial(m+N-1, N) : marg ? m^N-1 : m^N)
+        end
+    end
+    # center of the polytope
+    o = zeros(T, size(p))
+    if marg
+        o[end] = one(T)
+    end
+    # create the LMO
+    lmo = FrankWolfe.SymmetricLMO(BellCorrelationsLMO(p; mode=mode, nb=nb, sym=false, marg=marg, use_array=use_array), reduce, inflate)
+    # choosing the point on the line between o and p according to the visibility v0
+    vp = reduce(v0 * p + (one(T) - v0) * o, lmo)
+    o = reduce(o, lmo)
+    p = reduce(p, lmo)
+    # vp = v0 * p + (one(T) - v0) * o # TODO replace
+    # useful to make f efficient
+    normp2 = dot(vp, vp) / 2
+    # weird syntax to enable the compiler to correctly understand the type
+    f = let vp = vp, normp2 = normp2
+        x -> normp2 + dot(x, x) / 2 - dot(vp, x)
+    end
+    grad! = let vp = vp
+        (storage, xit) -> begin
+            @inbounds for x in eachindex(xit)
+                storage[x] = xit[x] - vp[x]
+            end
+        end
+    end
+    if active_set === nothing
+        # run the LMO once from the center o to get a vertex
+        x0 = FrankWolfe.compute_extreme_point(lmo, o - vp)
+        active_set = FrankWolfe.ActiveSetQuadratic([(one(T), x0)], I, -vp)
+        lmo.lmo.active_set = active_set
+    else
+        if active_set isa ActiveSetStorage
+            active_set = load_active_set(active_set, T; sym=sym, marg=marg, use_array=use_array, reynolds=reynolds)
+        end
+        active_set_link_lmo!(active_set, lmo, -vp)
+        active_set_reinitialise!(active_set)
+        if verbose > 1
+            println("Active set initialised")
+        end
+    end
+    if verbose > 0
+        println()
+    end
+    callback = build_callback(
+        p,
+        v0,
+        o,
+        shr2^(iseven(N) ? N ÷ 2 : N / 2),
+        verbose,
+        epsilon,
+        callback_interval,
+        renorm_interval,
+        hyperplane_interval,
+        bound_interval,
+        nb_increment_interval,
+        save,
+        file,
+        save_interval,
+    )
+    # main call to FW
+    x, ds, primal, dual_gap, _, as = FrankWolfe.blended_pairwise_conditional_gradient(
+        f,
+        grad!,
+        lmo,
+        active_set;
+        callback=callback,
+        epsilon=epsilon,
+        lazy=lazy,
+        line_search=FrankWolfe.Shortstep(one(T)),
+        max_iteration=max_iteration,
+        recompute_last_vertex=recompute_last_vertex,
+        renorm_interval=typemax(Int),
+        trajectory=false,
+        verbose=false,
+        kwargs...,
+    )
+    if verbose ≥ 2
+        println()
+        @printf("Primal: %.2e\n", primal)
+        @printf("FW gap: %.2e\n", dual_gap)
+        @printf("#Atoms: %d\n", length(as))
+        @printf("  #LMO: %d\n", lmo.lmo.data[2])
+    end
+    atoms = [FrankWolfe.SymmetricArray(BellCorrelationsDS(atom.data; type=TL), TL.(atom.vec)) for atom in as.atoms]
+    vp_last = FrankWolfe.SymmetricArray(TL.(vp.data), TL.(vp.vec))
+    as = FrankWolfe.ActiveSetQuadratic([(TL.(as.weights[i]), atoms[i]) for i in eachindex(as)], I, -vp_last)
+    FrankWolfe.compute_active_set_iterate!(as)
+    x = as.x
+    tmp = 0 #abs(FrankWolfe.fast_dot(vp - x, p)) TODO
+    M = TL.((vp - x) / (tmp == 0 ? 1 : tmp))
+    if mode_last ≥ 0 # bypass the last LMO with a negative mode
+        lmo_last = FrankWolfe.SymmetricLMO(BellCorrelationsLMO(lmo.lmo; mode=mode_last, type=TL, nb=nb_last), reduce, inflate)
+        ds = FrankWolfe.compute_extreme_point(lmo_last, -M; verbose=verbose > 0)
+    else
+        ds = FrankWolfe.SymmetricArray(BellCorrelationsDS(ds.data; type=TL), TL.(ds.vec))
+    end
+    # renormalise the inequality by its smalles element, neglecting entries smaller than epsilon_last
+    if epsilon_last > 0
+        M[abs.(M) .< epsilon_last] .= zero(TL)
+        M ./= minimum(abs.(M[abs.(M) .> zero(TL)]))
+    end
+    β = FrankWolfe.fast_dot(M, ds) / FrankWolfe.fast_dot(M, p) # local/global max found by the LMO
+    dual_gap = FrankWolfe.fast_dot(x - vp, x) - FrankWolfe.fast_dot(x - vp, ds)
+    if verbose > 0
+        if verbose ≥ 2 && mode_last ≥ 0
+            @printf("FW gap: %.2e\n", dual_gap) # recomputed FW gap (usually with a more reliable heuristic)
+            println()
+        end
+        if primal > dual_gap
+            @printf("v_c ≤ %f\n", β)
+        else
+            ν = 1 / (1 + norm(vp - as.x, 2))
+            @printf("v_c ≥ %f (%f)\n", shr2^(N / 2) * ν * v0, shr2^(N / 2) * v0)
+        end
+    end
+    if save
+        serialize(file * ".dat", ActiveSetStorage(as))
+    end
+    return x, ds, primal, dual_gap, as, M, β
+end
+
+"""
+Compute the local bound of a Bell inequality parametrised by `M`.
+No symmetry detection is implemented yet, used mostly for pedagogy and tests.
+"""
+function local_bound_correlation(
+    M::Array{T, N};
+    marg::Bool=false,
+    mode::Int=1,
+    sym::Bool=false,
+    nb::Int=10^5,
+    verbose=false,
+) where {T <: Number} where {N}
+    ds = FrankWolfe.compute_extreme_point(BellCorrelationsLMO(M; marg=marg, mode=mode, sym=sym, nb=nb), -M; verbose=verbose)
+    return FrankWolfe.fast_dot(M, ds), ds
+end
+
+"""
+Compute the nonlocality threshold of the qubit measurements encoded by the Bloch vectors `vec` in a Bell scenario with `N` parties.
+
+Arguments:
+ - `vec`: an `m × 3` matrix with Bloch vectors coordinates,
+ - `N`: the number of parties.
+
+Returns:
+ - `lower_bound_infinite`: a lower bound on the nonlocality threshold under all projective measurements (in the subspace spanned by `vec` in the Bloch sphere),
+ - `lower_bound`: a lower bound on the nonlocality threshold under the measurements provided in input,
+ - `upper_bound`: a (heuristic) upper bound on the nonlocality threshold under the measurements provided in input, also valid for all projective measurements
+ - `local_model`: a decomposition of the correlation tensor obtained by applying the measurements encoded by the Bloch vectors `vec` on all `N` subsystems of the shared state `rho` with visibility `lower_bound`,
+ - `bell_inequality`: a (heuristic) Bell inequality corresponding to `upper_bound`.
+
+Optional arguments:
+ - `rho`: the shared state, by default the singlet state in the bipartite case and the GHZ state otherwise,
+ - `v0`: the initial visibility, which should be an upper bound on the nonlocality threshold, 1.0 by default,
+ - `precision`: number of digits of `lower_bound`, 4 by default,
+ - for the other optional arguments, see `bell_frank_wolfe`.
+"""
+function nonlocality_threshold_correlation(
+    vec::Union{TB, Vector{TB}},
+    N::Int;
+    rho=N == 2 ? rho_singlet(; type=T) : rho_GHZ(N; type=T),
+    epsilon=1e-8,
+    marg::Bool=false,
+    v0=one(T),
+    precision=4,
+    verbose=-1,
+    kwargs...,
+) where {TB <: AbstractMatrix{T}} where {T <: Number}
+    p = correlation_tensor(vec, N; rho=rho, marg=marg)
+    shr2 = shrinking_squared(vec; verbose=verbose > 0)
+    lower_bound = zero(T)
+    upper_bound = one(T)
+    local_model = nothing
+    bell_inequality = nothing
+    while upper_bound - lower_bound > 10.0^(-precision)
+        res = bell_frank_wolfe_correlation(
+            p;
+            v0=v0,
+            verbose=verbose + (upper_bound == one(T)) / 2,
+            epsilon=epsilon,
+            shr2=shr2,
+            marg=marg,
+            kwargs...,
+        )
+        x, ds, primal, dual_gap, as, M, β = res
+        if primal > 10epsilon && dual_gap > 10epsilon
+            @warn "Please increase nb or max_iteration"
+        end
+        if dual_gap < primal
+            if β < upper_bound
+                upper_bound = round(β; digits=precision)
+                bell_inequality = M
+                if v0 == round(β; digits=precision)
+                    v0 = round(β - 10.0^(-precision); digits=precision)
+                else
+                    v0 = round(β; digits=precision)
+                end
+            else
+                @warn "Unexpected output"
+                break
+            end
+        else
+            lower_bound = v0
+            local_model = as
+            if upper_bound < lower_bound
+                upper_bound = round(v0 + 2 * 10.0^(-precision); digits=precision)
+            end
+            v0 = (lower_bound + upper_bound) / 2
+        end
+    end
+    o = zeros(T, size(p))
+    if marg
+        o[end] = one(T)
+    end
+    ν = 1 / (1 + norm(lower_bound * p + (1 - lower_bound) * o - local_model.x, 2))
+    lower_bound_infinite = shr2^(N / 2) * ν * lower_bound
+    # when mode_last = 0, the upper bound is not valid until the actual local bound (and not only the heuristic one) is computed
+    return lower_bound_infinite, lower_bound, upper_bound, local_model, bell_inequality
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,37 +6,37 @@ using Test
 
 @testset "Testing the local bound exact computation for various scenarios " begin
     Random.seed!(0)
-    @test local_bound(rand(-10:10, (10, 10)))[1] == 251
+    @test local_bound_correlation(rand(-10:10, (10, 10)))[1] == 251
     Random.seed!(0)
-    @test local_bound(rand(-10:10, (6, 6, 6)))[1] == 379
+    @test local_bound_correlation(rand(-10:10, (6, 6, 6)))[1] == 379
     Random.seed!(0)
-    @test local_bound(rand(-10:10, (4, 4, 4, 4)))[1] == 354
+    @test local_bound_correlation(rand(-10:10, (4, 4, 4, 4)))[1] == 354
     Random.seed!(0)
-    @test local_bound(rand(-10:10, (3, 3, 3, 3, 3)))[1] == 303
+    @test local_bound_correlation(rand(-10:10, (3, 3, 3, 3, 3)))[1] == 303
 end
 
 @testset "Testing Bell-Frank-Wolfe with d=2, N=2, and correlation matrices" begin
     # HQVNB17 without symmetry
-    @test abs(bell_frank_wolfe(correlation_matrix(HQVNB17_vec(3)); sym=false, use_array=false)[3] - 0.613691) < 1e-5
-    @test abs(bell_frank_wolfe(correlation_matrix(HQVNB17_vec(3)); sym=false, use_array=true)[3] - 0.613691) < 1e-5
+    @test abs(bell_frank_wolfe_correlation(correlation_matrix(HQVNB17_vec(3)); sym=false, use_array=false)[3] - 0.613691) < 1e-5
+    @test abs(bell_frank_wolfe_correlation(correlation_matrix(HQVNB17_vec(3)); sym=false, use_array=true)[3] - 0.613691) < 1e-5
     # HQVNB17 with symmetry
-    @test abs(bell_frank_wolfe(correlation_matrix(HQVNB17_vec(3)); use_array=false)[3] - 0.613691) < 1e-5
-    @test abs(bell_frank_wolfe(correlation_matrix(HQVNB17_vec(3)); use_array=true)[3] - 0.613691) < 1e-5
-    @test abs(bell_frank_wolfe(correlation_matrix(HQVNB17_vec(5)))[3] - 5.69) < 1e-1
+    @test abs(bell_frank_wolfe_correlation(correlation_matrix(HQVNB17_vec(3)); use_array=false)[3] - 0.613691) < 1e-5
+    @test abs(bell_frank_wolfe_correlation(correlation_matrix(HQVNB17_vec(3)); use_array=true)[3] - 0.613691) < 1e-5
+    @test abs(bell_frank_wolfe_correlation(correlation_matrix(HQVNB17_vec(5)))[3] - 5.69) < 1e-1
 end
 
 @testset "Testing Bell-Frank-Wolfe with d=2, N=2, and cor/marg matrices   " begin
     # using the exact LMO at the end
     @test abs(
-        bell_frank_wolfe(correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true); marg=true, mode_last=1)[3] -
+        bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true); marg=true, mode_last=1)[3] -
         0.613691,
     ) < 1e-5
     # with warm start
     @test abs(
-        bell_frank_wolfe(
+        bell_frank_wolfe_correlation(
             correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true);
             marg=true,
-            active_set=bell_frank_wolfe(
+            active_set=bell_frank_wolfe_correlation(
                 correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true);
                 marg=true,
                 max_iteration=10,
@@ -44,11 +44,11 @@ end
         )[3] - 0.613691,
     ) < 1e-5
     @test abs(
-        bell_frank_wolfe(
+        bell_frank_wolfe_correlation(
             correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true);
             marg=true,
             active_set=BellPolytopes.ActiveSetStorage(
-                bell_frank_wolfe(
+                bell_frank_wolfe_correlation(
                     correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true);
                     marg=true,
                     max_iteration=10,
@@ -59,7 +59,7 @@ end
 
     # HQVNB17 without symmetry
     @test abs(
-        bell_frank_wolfe(
+        bell_frank_wolfe_correlation(
             correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true);
             marg=true,
             sym=false,
@@ -67,7 +67,7 @@ end
         )[3] - 0.613691,
     ) < 1e-5
     @test abs(
-        bell_frank_wolfe(
+        bell_frank_wolfe_correlation(
             correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true);
             marg=true,
             sym=false,
@@ -76,21 +76,21 @@ end
     ) < 1e-5
     # HQVNB17 with symmetry
     @test abs(
-        bell_frank_wolfe(correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true); marg=true, use_array=false)[3] -
+        bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true); marg=true, use_array=false)[3] -
         0.613691,
     ) < 1e-5
     @test abs(
-        bell_frank_wolfe(correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true); marg=true, use_array=true)[3] -
+        bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(3), 2; rho=rho_GHZ(2), marg=true); marg=true, use_array=true)[3] -
         0.613691,
     ) < 1e-5
-    @test abs(bell_frank_wolfe(correlation_tensor(HQVNB17_vec(5), 2; rho=rho_GHZ(2), marg=true); marg=true)[3] - 5.69) < 1e-1
-    #  @test abs(bell_frank_wolfe(correlation_tensor(HQVNB17_vec(7), 2; rho=rho_GHZ(2), marg=true); marg=true)[3] - 23.02) < 1e-1
+    @test abs(bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(5), 2; rho=rho_GHZ(2), marg=true); marg=true)[3] - 5.69) < 1e-1
+    #  @test abs(bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(7), 2; rho=rho_GHZ(2), marg=true); marg=true)[3] - 23.02) < 1e-1
 end
 
 @testset "Testing Bell-Frank-Wolfe with d=2, N=3, and correlation tensors " begin
     # HQVNB17 without symmetry
     @test abs(
-        bell_frank_wolfe(
+        bell_frank_wolfe_correlation(
             correlation_tensor(HQVNB17_vec(3), 3; rho=rho_W(3), marg=false);
             sym=false,
             use_array=false,
@@ -98,7 +98,7 @@ end
         )[3] - 5.89838,
     ) < 1e-5
     @test abs(
-        bell_frank_wolfe(
+        bell_frank_wolfe_correlation(
             correlation_tensor(HQVNB17_vec(3), 3; rho=rho_W(3), marg=false);
             sym=false,
             use_array=true,
@@ -107,11 +107,11 @@ end
     ) < 1e-5
     # HQVNB17 with symmetry
     @test abs(
-        bell_frank_wolfe(correlation_tensor(HQVNB17_vec(3), 3; rho=rho_W(3), marg=false); use_array=false, marg=false)[3] -
+        bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(3), 3; rho=rho_W(3), marg=false); use_array=false, marg=false)[3] -
         5.89838,
     ) < 1e-5
     @test abs(
-        bell_frank_wolfe(correlation_tensor(HQVNB17_vec(3), 3; rho=rho_W(3), marg=false); use_array=true, marg=false)[3] -
+        bell_frank_wolfe_correlation(correlation_tensor(HQVNB17_vec(3), 3; rho=rho_W(3), marg=false); use_array=true, marg=false)[3] -
         5.89838,
     ) < 1e-5
 end
